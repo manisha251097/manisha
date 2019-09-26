a8da237da9c6692ec5cca05091c49ac23da64795
manisha251097 <manishasalavadi9514@msitprogram.net>
2019-09-26 17:07:41 +0530

    22

---
+++ day 4/ass 1/Contact.java
@@ -0,0 +1,24 @@
+/**
+ * This class discusses about the Contact Object. The attributes of the contact object
+ * are as follows.
+ * name : Name of the person / friend.
+ * email : mail id of the person / friend.
+ * phoneNumber : Phone Number of the person / friend.
+ * 
+ * @author Siva Sankar
+ */
+
+//  Your code goes here... For Contact class
+class contact{
+    private String name;
+    private String email;
+    private String phoneNumber;
+    
+    
+    contact(final String n,final String e,final String p){
+        this.name= n;
+        this.email= e;
+        this.phoneNumber= p;
+    }
+}
+
