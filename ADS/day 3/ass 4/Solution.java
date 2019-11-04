97065705e22f63c87e856bbc268cc9c827cf2e5d
manisha251097 <manishasalavadi9514@msitprogram.net>
2019-11-04 10:34:07 +0530

    updated

--- ADS/day 3/ass 4/Solution.java
+++ ADS/day 3/ass 4/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
 	public Team[] sort(Team[] teams) {
 		// your code goes here
-		//  The teams in the Team array are sorted
+		// The teams in the Team array are sorted
 		for (int i = 0; i < teams.length - 1; i++) {
 			int min = i;
 			for (int j = i + 1; j < teams.length; j++) {
@@ -17,6 +17,7 @@ class Solution {
 	}
 }
 
+// comparable method compares two objects
 class Team implements Comparable<Team> {
 	String teamName;
 	int noOfWins;
@@ -31,14 +32,14 @@ class Team implements Comparable<Team> {
 	}
 
 	public String toString() {
-		                        // retruns all the attributes as a string but appending with ", "
+		// retruns all the attributes as a string but appending with ", "
 		return "";
 	}
 
 	/**
 	 * 
-	 * Two teams wins, losses, draws are compared and returns 1 or -1
-	 * if none of the condition satisfied it returns 0
+	 * Two teams wins, losses, draws are compared and returns 1 or -1 if none of the
+	 * condition satisfied it returns 0
 	 * 
 	 */
 	public int compareTo(Team that) {
