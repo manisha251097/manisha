135c9716f2f7e62bcae0fc6ea0caa6a08ddafd17
manisha251097 <manishasalavadi9514@msitprogram.net>
2019-11-10 12:04:16 +0530

    Assgn

--- ADS/day8/day6/asgn2/SequentialSearchST.java
+++ ADS/day8/day6/asgn2/SequentialSearchST.java
@@ -1,19 +1,20 @@
 /**
  * Sequential search symbol table.
- * @author Prasanna.
+ * 
+ * @author manisha.
  */
-public class SequentialSearchST<Key extends Comparable<Key>, Value> {           
-    private Node first;      
+public class SequentialSearchST<Key extends Comparable<Key>, Value> {
+    private Node first;
 
     private class Node {
         private Key key;
         private Value val;
         private Node next;
 
-        public Node(Key key, Value val)  {
+        public Node(Key key, Value val) {
             this.key = key;
             this.val = val;
-            
+
         }
     }
 
@@ -25,10 +26,10 @@ public class SequentialSearchST<Key extends Comparable<Key>, Value> {
 
     /**
      * Returns true if this symbol table contains the specified key.
-     * @param  key the key
-     * @return true if this symbol table contains key.
-     * false otherwise.
-     * Complexity:O(n).
+     * 
+     * @param key the key
+     * @return true if this symbol table contains key. false otherwise.
+     *         Complexity:O(n).
      */
     public boolean contains(Key key) {
         return get(key) != null;
@@ -36,20 +37,20 @@ public class SequentialSearchST<Key extends Comparable<Key>, Value> {
 
     /**
      * Returns the value associated with the given key in this symbol table.
-     * @param  key the key
-     * @return the value associated with the given key
-     * Complexity:O(n).
+     * 
+     * @param key the key
+     * @return the value associated with the given key Complexity:O(n).
      */
     public Value get(Key key) {
         if (key == null) {
             return null;
         }
-        if(first.key.compareTo(key) == 0){
+        if (first.key.compareTo(key) == 0) {
             return exchange(first);
         }
         Node current = first;
-        while(current != null) {
-            if(current.next.key.compareTo(key) == 0){
+        while (current != null) {
+            if (current.next.key.compareTo(key) == 0) {
                 return exchange(current);
             }
             current = current.next;
@@ -59,9 +60,9 @@ public class SequentialSearchST<Key extends Comparable<Key>, Value> {
 
     /**
      * Inserts the specified key-value pair into the symbol table
-     * @param  key the key
-     * @param  val the value
-     * Complexity:O(n).
+     * 
+     * @param key the key
+     * @param val the value Complexity:O(n).
      */
     public void put(Key key, Value val) {
         Node node = new Node(key, val);
@@ -70,48 +71,49 @@ public class SequentialSearchST<Key extends Comparable<Key>, Value> {
             return;
         }
         Node current = first;
-        while(current != null){
-            if(current.key.compareTo(key)==0){
+        while (current != null) {
+            if (current.key.compareTo(key) == 0) {
                 current.val = val;
                 return;
             }
             current = current.next;
         }
         Node oldFirst = first;
-        first = new Node(key,val);
-        first.next = oldFirst;        
+        first = new Node(key, val);
+        first.next = oldFirst;
     }
+
     /**
      * exchange method
-     * @param current current node.
-     * complexity:O(n).
+     * 
+     * @param current current node. complexity:O(n).
      */
 
-    private Value exchange(Node current){
+    private Value exchange(Node current) {
         Node temp = current.next;
         Value val = temp.val;
         current.next = current.next.next;
         Node node = first;
-        while(node.next != null) {
+        while (node.next != null) {
             node = node.next;
         }
         node.next = temp;
         temp.next = null;
         return val;
     }
+
     /**
      * Returns all keys in the symbol table as an Iterable.
-     * @return all keys in the symbol table
-     * Complexity:O(n).
+     * 
+     * @return all keys in the symbol table Complexity:O(n).
      */
-    public Iterable<Key> keys()  {
+    public Iterable<Key> keys() {
         ResizingArrayQueue<Key> queue = new ResizingArrayQueue<Key>();
         Node current = first;
-        while(current != null) {
+        while (current != null) {
             queue.enqueue(current.key);
             current = current.next;
         }
         return queue;
     }
 }
- 
