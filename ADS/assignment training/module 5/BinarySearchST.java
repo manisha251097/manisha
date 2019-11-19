5549b0a0114c805294556c22b009f8f798882992
manisha251097 <manishasalavadi9514@msitprogram.net>
2019-11-19 18:11:49 +0530

    updated

---
+++ ADS/assignment training/module 5/BinarySearchST.java
@@ -0,0 +1,143 @@
+import java.util.NoSuchElementException;
+import java.util.Arrays;
+
+/**
+ * @author Manisha
+ */
+public class BinarySearchST<Key extends Comparable<Key>, Value> {
+    private static final int INIT_CAPACITY = 2;
+    private Key[] keys;
+    private Value[] vals;
+    private int n = 0;
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public BinarySearchST() {
+        this(INIT_CAPACITY);
+    }
+
+    /**
+     * Initializes an empty symbol table with the specified initial capacity.
+     * 
+     * @param capacity the maximum capacity
+     */
+    public BinarySearchST(int capacity) {
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+
+    // resize the underlying arrays
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Key[] tempk = (Key[]) new Comparable[capacity];
+        Value[] tempv = (Value[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            tempk[i] = keys[i];
+            tempv[i] = vals[i];
+        }
+        vals = tempv;
+        keys = tempk;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty; {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns the number of keys in this symbol table strictly less than
+     * {@code key}.
+     *
+     * @param key the key
+     * @return the number of keys in the symbol table strictly less than {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public int rank(Key key) {
+        if (key == null)
+            throw new IllegalArgumentException("argument to rank() is null");
+
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0)
+                hi = mid - 1;
+            else if (cmp > 0)
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return lo;
+    }
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the
+     * old value with the new value if the symbol table already contains the
+     * specified key. Deletes the specified key (and its associated value) from this
+     * symbol table if the specified value is {@code null}.
+     *
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val) {
+        if (key == null)
+            throw new IllegalArgumentException("first argument to put() is null");
+
+        int i = rank(key);
+
+        if (!isEmpty() && keys[n - 1].compareTo(key) < 0) {
+            keys[n] = key;
+            vals[n] = val;
+            n++;
+        }
+
+        // key is already in table
+        if (i < n && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+
+        // insert new key-value pair
+        if (n == keys.length)
+            resize(2 * keys.length);
+
+        for (int j = n; j > i; j--) {
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+
+    }
+
+    /**
+     * Unit tests the {@code BinarySearchST} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        BinarySearchST<String, Integer> st = new BinarySearchST<String, Integer>();
+        st.put("Q", 2);
+        st.put("A", 3);
+        st.put("C", 6);
+        st.put("G",7);
+        System.out.println(Arrays.toString(st.keys));
+    }
+}
+ 
